3af6b07214142d86916f6b9a2c753210718355e1
smarkwal <stephan@markwalder.net>
2022-02-28 15:44:01 +0100

    Code: Use hardcoded version information for JAPICC.

--- react-app/src/components/japicc/JapiccAbout.js
+++ react-app/src/components/japicc/JapiccAbout.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import JapiccVersion from "./JapiccVersion";
 
 const JapiccAbout = () => {
 	return (<div className="alert alert-secondary mt-5">
 		<h5 className="alert-heading">About Java API Compliance Checker</h5>
 		<hr/>
-		<JapiccVersion/>
+		<pre>{`Java API Compliance Checker (JAPICC) 2.4
+Copyright (C) 2018 Andrey Ponomarenko's ABI Laboratory
+License: LGPLv2.1+ <http://www.gnu.org/licenses/>
+This program is free software: you can redistribute it and/or modify it.
+
+Written by Andrey Ponomarenko.`}</pre>
 		<hr/>
 		<a className="alert-link" href="https://github.com/lvc/japi-compliance-checker">https://github.com/lvc/japi-compliance-checker</a>
 	</div>);
