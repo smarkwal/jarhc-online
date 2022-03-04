6b25d355d8daeb2d53b37e8b4077745b570fe978
smarkwal <stephan@markwalder.net>
2022-03-04 20:21:17 +0100

    Code: Use HashRouter instead of BrowserRouter.

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import './index.css';
 import App from './App';
 import JDiff from "./JDiff";
@@ -10,14 +10,14 @@ import reportWebVitals from './reportWebVitals';
 
 let rootElement = document.getElementById('root');
 ReactDOM.render(<React.StrictMode>
-	<BrowserRouter>
+	<HashRouter>
 		<Routes>
 			<Route path="/" element={<App/>}/>
 			<Route path="japicc" element={<JAPICC/>}/>
 			<Route path="jarhc" element={<JarHC/>}/>
 			<Route path="jdiff" element={<JDiff/>}/>
 		</Routes>
-	</BrowserRouter>
+	</HashRouter>
 </React.StrictMode>, rootElement);
 
 // If you want to start measuring performance in your app, pass a function
