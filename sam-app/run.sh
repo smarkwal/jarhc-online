f0ef224d6c64103950a6168e80504450fac0fed6
smarkwal <stephan@markwalder.net>
2022-03-06 11:44:19 +0100

    Code: Use Makefile for SAM app.

--- sam-app/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sam validate || exit 1
-sam build || exit 2
-sam local start-api --port 3001 --parameter-overrides "ApiDomain=localhost:3001 WebsiteDomain=localhost:3000 WebsiteURL=http://localhost:3000"
