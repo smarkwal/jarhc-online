b98558186cb29396505a0e5692105fea418a24f4
smarkwal <stephan@markwalder.net>
2022-03-06 19:59:18 +0100

    Code: Move images into separate folder.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,13 +9,13 @@ function App() {
 		</div>
 		<div className="row">
 			<div className="col-12 col-md-6 col-lg-4 mb-4">
-				<Card imageUrl="/card-image-japicc.png" title="JAPICC - Java API Compliance Checker" text="Compare two versions of a Java library and get a report with details on source and binary compatibility." linkUrl="/japicc" linkText="Start"/>
+				<Card imageUrl="/img/card-image-japicc.png" title="JAPICC - Java API Compliance Checker" text="Compare two versions of a Java library and get a report with details on source and binary compatibility." linkUrl="/japicc" linkText="Start"/>
 			</div>
 			<div className="col-12 col-md-6 col-lg-4 mb-4">
-				<Card imageUrl="/card-image-jarhc.png" title="JarHC - JAR Health Check" text="Analyze a set of Java libraries for their binary compatibility." linkUrl="/jarhc" linkText="Start"/>
+				<Card imageUrl="/img/card-image-jarhc.png" title="JarHC - JAR Health Check" text="Analyze a set of Java libraries for their binary compatibility." linkUrl="/jarhc" linkText="Start"/>
 			</div>
 			<div className="col-12 col-md-6 col-lg-4 mb-4">
-				<Card imageUrl="/card-image-jdiff.png" title="jDiff - Java API Diff" text="" linkUrl="/jdiff" linkText="Start"/>
+				<Card imageUrl="/img/card-image-jdiff.png" title="jDiff - Java API Diff" text="" linkUrl="/jdiff" linkText="Start"/>
 			</div>
 		</div>
 	</>);
