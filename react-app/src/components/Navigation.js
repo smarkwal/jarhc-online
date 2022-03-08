b57f8f18eabee438360d7830fc4b7a27ddd004f7
smarkwal <stephan@markwalder.net>
2022-03-08 22:37:35 +0100

    React: Update website content.

--- react-app/src/components/Navigation.js
+++ react-app/src/components/Navigation.js
@@ -3,17 +3,22 @@ import {NavLink} from "react-router-dom";
 import Auth from "./Auth";
 
 const Navigation = () => {
-	return (<nav className="navbar navbar-expand navbar-dark bg-dark mb-4" style={{fontSize: "1.5rem"}}>
-		<div className="navbar-nav me-auto ps-4">
-			<NavLink className="nav-item nav-link px-3" activeclassname="active" to="/">Home</NavLink>
-			<NavLink className="nav-item nav-link px-3" activeclassname="active" to="/japicc">JAPICC</NavLink>
-			<NavLink className="nav-item nav-link px-3" activeclassname="active" to="/jarhc">JarHC</NavLink>
-			<NavLink className="nav-item nav-link px-3" activeclassname="active" to="/jardiff">JarDiff</NavLink>
-			<NavLink className="nav-item nav-link px-3" activeclassname="active" to="/srcdiff">SrcDiff</NavLink>
+	return (<nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4" style={{fontSize: "1.5rem"}}>
+		<button className="navbar-toggler ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span className="navbar-toggler-icon"/>
+		</button>
+		<div className="collapse navbar-collapse" id="navbarSupportedContent">
+			<div className="navbar-nav me-auto ps-4">
+				<NavLink className="nav-item nav-link px-3" activeclassname="active" to="/">Home</NavLink>
+				<NavLink className="nav-item nav-link px-3" activeclassname="active" to="/japicc">JAPICC</NavLink>
+				<NavLink className="nav-item nav-link px-3" activeclassname="active" to="/jarhc">JarHC</NavLink>
+				<NavLink className="nav-item nav-link px-3" activeclassname="active" to="/jardiff">JarDiff</NavLink>
+				<NavLink className="nav-item nav-link px-3" activeclassname="active" to="/srcdiff">SrcDiff</NavLink>
+			</div>
 		</div>
-		<div className="pe-4">
+		<div className="d-flex pe-4">
 			{Auth.isSignedIn() ? <button className="btn btn-primary" onClick={Auth.signOut}>Sign out</button> : <div>
-				<button className="btn btn-primary me-3" onClick={Auth.signIn}>Sign in</button>
+				<button className="btn btn-primary ms-3 me-3" onClick={Auth.signIn}>Sign in</button>
 				<button className="btn btn-primary" onClick={Auth.signUp}>Sign up</button>
 			</div>}
 		</div>
