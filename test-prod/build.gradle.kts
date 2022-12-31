10a71fc47cdcc6e714b62062e86ce57c35afd6d4
smarkwal <stephan@markwalder.net>
2022-12-31 13:46:16 +0100

    Bump AWS Java SDK 1 from 1.12.372 to 1.12.376, AWS Java SDK 2 from 2.19.4 to 2.19.8, and Mockito from 4.10.0 to 4.11.0.

--- test-prod/build.gradle.kts
+++ test-prod/build.gradle.kts
@@ -49,8 +49,8 @@ dependencies {
 
     // BOMs for version constraints
     implementation(platform("org.junit:junit-bom:5.9.1"))
-    implementation(platform("software.amazon.awssdk:bom:2.19.4"))
-    implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.372"))
+    implementation(platform("software.amazon.awssdk:bom:2.19.8"))
+    implementation(platform("com.amazonaws:aws-java-sdk-bom:1.12.376"))
 
     // test libraries
     implementation("org.junit.jupiter:junit-jupiter")
