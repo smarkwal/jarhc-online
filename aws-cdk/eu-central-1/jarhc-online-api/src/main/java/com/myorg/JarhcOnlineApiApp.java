4edff5b0b8bcbe0aff6c79adaea8109d78cd1390
smarkwal <stephan@markwalder.net>
2024-10-13 00:01:20 +0200

    Project: AWS CDK: Fix and reformat Java code.

--- aws-cdk/eu-central-1/jarhc-online-api/src/main/java/com/myorg/JarhcOnlineApiApp.java
+++ aws-cdk/eu-central-1/jarhc-online-api/src/main/java/com/myorg/JarhcOnlineApiApp.java
@@ -4,39 +4,23 @@ import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 
-import java.util.Arrays;
-
 public class JarhcOnlineApiApp {
-    public static void main(final String[] args) {
-        App app = new App();
 
-        new JarhcOnlineApiStack(app, "jarhc-online-api", StackProps.builder()
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
+	public static void main(final String[] args) {
 
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-                /*
-                .env(Environment.builder()
-                        .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
-                        .region(System.getenv("CDK_DEFAULT_REGION"))
-                        .build())
-                */
+		App app = new App();
+		new JarhcOnlineApiStack(app, "jarhc-online-api",
+				StackProps.builder()
+						.env(
+								Environment.builder()
+										.account("837783538267")
+										.region("eu-central-1")
+										.build()
+						)
+						.build()
+		);
+		app.synth();
 
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /*
-                .env(Environment.builder()
-                        .account("123456789012")
-                        .region("us-east-1")
-                        .build())
-                */
+	}
 
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-                .build());
-
-        app.synth();
-    }
 }
-
--- aws-cdk/eu-central-1/jarhc-online-website/src/main/java/com/myorg/JarhcOnlineWebsiteStack.java
+++ aws-cdk/eu-central-1/jarhc-online-website/src/main/java/com/myorg/JarhcOnlineWebsiteStack.java
@@ -1,171 +1,205 @@
 package com.myorg;
 
-import software.constructs.Construct;
-
-import java.util.*;
-import software.amazon.awscdk.CfnMapping;
-import software.amazon.awscdk.CfnTag;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import software.amazon.awscdk.CfnOutput;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-
-import software.amazon.awscdk.*;
-import software.amazon.awscdk.services.cloudfront.*;
-import software.amazon.awscdk.services.route53.*;
-import software.amazon.awscdk.services.s3.*;
+import software.amazon.awscdk.services.cloudfront.CfnDistribution;
+import software.amazon.awscdk.services.cloudfront.CfnOriginAccessControl;
+import software.amazon.awscdk.services.route53.CfnRecordSetGroup;
+import software.amazon.awscdk.services.s3.CfnBucket;
+import software.amazon.awscdk.services.s3.CfnBucketPolicy;
+import software.constructs.Construct;
 
 class JarhcOnlineWebsiteStack extends Stack {
-    private Object websiteCloudFrontDistributionUrl;
 
-    public Object getWebsiteCloudFrontDistributionUrl() {
-        return this.websiteCloudFrontDistributionUrl;
-    }
+	private Object websiteCloudFrontDistributionUrl;
 
-    public JarhcOnlineWebsiteStack(final Construct scope, final String id) {
-        super(scope, id, null);
-    }
+	public Object getWebsiteCloudFrontDistributionUrl() {
+		return this.websiteCloudFrontDistributionUrl;
+	}
 
-    public JarhcOnlineWebsiteStack(final Construct scope, final String id, final StackProps props) {
-        this(scope, id, props, null, null, null, null);
-    }
+	public JarhcOnlineWebsiteStack(final Construct scope, final String id) {
+		super(scope, id, null);
+	}
 
-    public JarhcOnlineWebsiteStack(final Construct scope, final String id, final StackProps props,
-            String websiteDomain,
-            String websiteCertificateArn,
-            String websiteLogsBucket,
-            String dnsZoneId) {
-        super(scope, id, props);
+	public JarhcOnlineWebsiteStack(final Construct scope, final String id, final StackProps props) {
+		this(scope, id, props, null, null, null, null);
+	}
 
-        websiteDomain = Optional.ofNullable(websiteDomain).isPresent() ? websiteDomain
-                : "online.jarhc.org";
-        websiteCertificateArn = Optional.ofNullable(websiteCertificateArn).isPresent() ? websiteCertificateArn
-                : "arn:aws:acm:us-east-1:837783538267:certificate/3fa0002b-0667-4e67-b8c5-d228a4341811";
-        websiteLogsBucket = Optional.ofNullable(websiteLogsBucket).isPresent() ? websiteLogsBucket
-                : "aws-cloud-reports.s3.amazonaws.com";
-        dnsZoneId = Optional.ofNullable(dnsZoneId).isPresent() ? dnsZoneId
-                : "Z39VOOPW73P7H0";
+	public JarhcOnlineWebsiteStack(final Construct scope, final String id, final StackProps props,
+	                               String websiteDomain,
+	                               String websiteCertificateArn,
+	                               String websiteLogsBucket,
+	                               String dnsZoneId) {
+		super(scope, id, props);
 
-        CfnBucket websiteBucket = CfnBucket.Builder.create(this, "WebsiteBucket")
-                .bucketName(websiteDomain)
-                .publicAccessBlockConfiguration(CfnBucket.PublicAccessBlockConfigurationProperty.builder()
-                        .blockPublicAcls(true)
-                        .blockPublicPolicy(true)
-                        .ignorePublicAcls(true)
-                        .restrictPublicBuckets(true)
-                        .build())
-                .lifecycleConfiguration(CfnBucket.LifecycleConfigurationProperty.builder()
-                        .rules(Arrays.asList(
-                                CfnBucket.RuleProperty.builder()
-                                        .id("AutoCleanup")
-                                        .status("Enabled")
-                                        .prefix("reports/")
-                                        .expirationInDays(30)
-                                        .build()))
-                        .build())
-                .loggingConfiguration(CfnBucket.LoggingConfigurationProperty.builder()
-                        .destinationBucketName("aws-cloud-reports")
-                        .logFilePrefix("s3-access-logs/")
-                        .build())
-                .build();
+		websiteDomain = Optional.ofNullable(websiteDomain).isPresent() ? websiteDomain : "online.jarhc.org";
+		websiteCertificateArn = Optional.ofNullable(websiteCertificateArn).isPresent() ? websiteCertificateArn : "arn:aws:acm:us-east-1:837783538267:certificate/3fa0002b-0667-4e67-b8c5-d228a4341811";
+		websiteLogsBucket = Optional.ofNullable(websiteLogsBucket).isPresent() ? websiteLogsBucket : "aws-cloud-reports.s3.amazonaws.com";
+		dnsZoneId = Optional.ofNullable(dnsZoneId).isPresent() ? dnsZoneId : "Z39VOOPW73P7H0";
 
-        websiteBucket.addMetadata("SamResourceId", "WebsiteBucket");
+		CfnBucket websiteBucket = CfnBucket.Builder.create(this, "WebsiteBucket")
+				.bucketName(websiteDomain)
+				.publicAccessBlockConfiguration(
+						CfnBucket.PublicAccessBlockConfigurationProperty.builder()
+								.blockPublicAcls(true)
+								.blockPublicPolicy(true)
+								.ignorePublicAcls(true)
+								.restrictPublicBuckets(true)
+								.build()
+				)
+				.lifecycleConfiguration(
+						CfnBucket.LifecycleConfigurationProperty.builder()
+								.rules(
+										List.of(
+												CfnBucket.RuleProperty.builder()
+														.id("AutoCleanup")
+														.status("Enabled")
+														.prefix("reports/")
+														.expirationInDays(30)
+														.build()
+										)
+								)
+								.build()
+				)
+				.loggingConfiguration(
+						CfnBucket.LoggingConfigurationProperty.builder()
+								.destinationBucketName("aws-cloud-reports")
+								.logFilePrefix("s3-access-logs/")
+								.build()
+				)
+				.build();
 
-        CfnOriginAccessControl websiteOriginAccessControl = CfnOriginAccessControl.Builder.create(this, "WebsiteOriginAccessControl")
-                .originAccessControlConfig(CfnOriginAccessControl.OriginAccessControlConfigProperty.builder()
-                        .name("WebsiteOriginAccessControl")
-                        .originAccessControlOriginType("s3")
-                        .signingBehavior("always")
-                        .signingProtocol("sigv4")
-                        .build())
-                .build();
+		websiteBucket.addMetadata("SamResourceId", "WebsiteBucket");
 
-        websiteOriginAccessControl.addMetadata("SamResourceId", "WebsiteOriginAccessControl");
+		CfnOriginAccessControl websiteOriginAccessControl = CfnOriginAccessControl.Builder.create(this, "WebsiteOriginAccessControl")
+				.originAccessControlConfig(
+						CfnOriginAccessControl.OriginAccessControlConfigProperty.builder()
+								.name("WebsiteOriginAccessControl")
+								.originAccessControlOriginType("s3")
+								.signingBehavior("always")
+								.signingProtocol("sigv4")
+								.build()
+				)
+				.build();
 
-        CfnDistribution websiteCloudFrontDistribution = CfnDistribution.Builder.create(this, "WebsiteCloudFrontDistribution")
-                .distributionConfig(CfnDistribution.DistributionConfigProperty.builder()
-                        .enabled(true)
-                        .aliases(Arrays.asList(
-                                websiteDomain))
-                        .httpVersion("http2")
-                        .origins(Arrays.asList(
-                                CfnDistribution.OriginProperty.builder()
-                                        .id("DefaultOrigin")
-                                        .domainName(websiteBucket.getAttrRegionalDomainName())
-                                        .originAccessControlId(websiteOriginAccessControl.getAttrId())
-                                        .s3OriginConfig(CfnDistribution.S3OriginConfigProperty.builder()
-                                                .originAccessIdentity("")
-                                                .build())
-                                        .build()))
-                        .defaultRootObject("index.html")
-                        .priceClass("PriceClass_100")
-                        .viewerCertificate(CfnDistribution.ViewerCertificateProperty.builder()
-                                .acmCertificateArn(websiteCertificateArn)
-                                .minimumProtocolVersion("TLSv1.2_2021")
-                                .sslSupportMethod("sni-only")
-                                .build())
-                        .defaultCacheBehavior(CfnDistribution.DefaultCacheBehaviorProperty.builder()
-                                .allowedMethods(Arrays.asList(
-                                        "GET",
-                                        "HEAD",
-                                        "OPTIONS"))
-                                .cachedMethods(Arrays.asList(
-                                        "GET",
-                                        "HEAD"))
-                                .cachePolicyId("4135ea2d-6df8-44a3-9df3-4b5a84be39ad")
-                                .responseHeadersPolicyId("67f7725c-6f97-4210-82d7-5512b31e9d03")
-                                .compress(true)
-                                .targetOriginId("DefaultOrigin")
-                                .viewerProtocolPolicy("redirect-to-https")
-                                .build())
-                        .logging(CfnDistribution.LoggingProperty.builder()
-                                .bucket(websiteLogsBucket)
-                                .prefix("cloudfront-access-logs/")
-                                .includeCookies(false)
-                                .build())
-                        .build())
-                .build();
+		websiteOriginAccessControl.addMetadata("SamResourceId", "WebsiteOriginAccessControl");
 
-        websiteCloudFrontDistribution.addMetadata("SamResourceId", "WebsiteCloudFrontDistribution");
+		CfnDistribution websiteCloudFrontDistribution = CfnDistribution.Builder.create(this, "WebsiteCloudFrontDistribution")
+				.distributionConfig(
+						CfnDistribution.DistributionConfigProperty.builder()
+								.enabled(true)
+								.aliases(List.of(websiteDomain))
+								.httpVersion("http2")
+								.origins(
+										List.of(
+												CfnDistribution.OriginProperty.builder()
+														.id("DefaultOrigin")
+														.domainName(websiteBucket.getAttrRegionalDomainName())
+														.originAccessControlId(websiteOriginAccessControl.getAttrId())
+														.s3OriginConfig(
+																CfnDistribution.S3OriginConfigProperty.builder()
+																		.originAccessIdentity("")
+																		.build()
+														)
+														.build()
+										)
+								)
+								.defaultRootObject("index.html")
+								.priceClass("PriceClass_100")
+								.viewerCertificate(
+										CfnDistribution.ViewerCertificateProperty.builder()
+												.acmCertificateArn(websiteCertificateArn)
+												.minimumProtocolVersion("TLSv1.2_2021")
+												.sslSupportMethod("sni-only")
+												.build()
+								)
+								.defaultCacheBehavior(
+										CfnDistribution.DefaultCacheBehaviorProperty.builder()
+												.allowedMethods(List.of("GET", "HEAD", "OPTIONS"))
+												.cachedMethods(List.of("GET", "HEAD"))
+												.cachePolicyId("4135ea2d-6df8-44a3-9df3-4b5a84be39ad")
+												.responseHeadersPolicyId("67f7725c-6f97-4210-82d7-5512b31e9d03")
+												.compress(true)
+												.targetOriginId("DefaultOrigin")
+												.viewerProtocolPolicy("redirect-to-https")
+												.build()
+								)
+								.logging(
+										CfnDistribution.LoggingProperty.builder()
+												.bucket(websiteLogsBucket)
+												.prefix("cloudfront-access-logs/")
+												.includeCookies(false)
+												.build()
+								)
+								.build()
+				)
+				.build();
 
-        CfnBucketPolicy websiteBucketPolicy = CfnBucketPolicy.Builder.create(this, "WebsiteBucketPolicy")
-                .bucket(websiteBucket.getRef())
-                .policyDocument(Map.of("Version", "2012-10-17",
-                "Statement", Arrays.asList(
-                        Map.of("Sid", "AllowCloudFrontServicePrincipal",
-                        "Effect", "Allow",
-                        "Principal", Map.of("Service", "cloudfront.amazonaws.com"),
-                        "Action", Arrays.asList(
-                                "s3:ListBucket",
-                                "s3:GetObject"),
-                        "Resource", Arrays.asList(
-                                "arn:aws:s3:::" + websiteBucket.getRef(),
-                                "arn:aws:s3:::" + websiteBucket.getRef() + "/*"),
-                        "Condition", Map.of("StringEquals", Map.of("AWS:SourceArn", "arn:aws:cloudfront::" + this.getAccount() + ":distribution/" + websiteCloudFrontDistribution.getAttrId()))))))
-                .build();
+		websiteCloudFrontDistribution.addMetadata("SamResourceId", "WebsiteCloudFrontDistribution");
 
-        websiteBucketPolicy.addMetadata("SamResourceId", "WebsiteBucketPolicy");
+		CfnBucketPolicy websiteBucketPolicy = CfnBucketPolicy.Builder.create(this, "WebsiteBucketPolicy")
+				.bucket(websiteBucket.getRef())
+				.policyDocument(Map.of(
+								"Version", "2012-10-17",
+								"Statement", List.of(
+										Map.of(
+												"Sid", "AllowCloudFrontServicePrincipal",
+												"Effect", "Allow",
+												"Principal", Map.of(
+														"Service", "cloudfront.amazonaws.com"
+												),
+												"Action", List.of(
+														"s3:ListBucket",
+														"s3:GetObject"
+												),
+												"Resource", List.of(
+														"arn:aws:s3:::" + websiteBucket.getRef(),
+														"arn:aws:s3:::" + websiteBucket.getRef() + "/*"
+												),
+												"Condition", Map.of(
+														"StringEquals", Map.of(
+																"AWS:SourceArn", "arn:aws:cloudfront::" + this.getAccount() + ":distribution/" + websiteCloudFrontDistribution.getAttrId()
+														)
+												)
+										)
+								)
+						)
+				)
+				.build();
 
-        CfnRecordSetGroup websiteDnsRecord = CfnRecordSetGroup.Builder.create(this, "WebsiteDnsRecord")
-                .hostedZoneId(dnsZoneId)
-                .recordSets(Arrays.asList(
-                        CfnRecordSetGroup.RecordSetProperty.builder()
-                                .name(websiteDomain)
-                                .type("A")
-                                .aliasTarget(CfnRecordSetGroup.AliasTargetProperty.builder()
-                                        .dnsName(websiteCloudFrontDistribution.getAttrDomainName())
-                                        .hostedZoneId("Z2FDTNDATAQYW2")
-                                        .evaluateTargetHealth(false)
-                                        .build())
-                                .build()))
-                .build();
+		websiteBucketPolicy.addMetadata("SamResourceId", "WebsiteBucketPolicy");
 
-        websiteDnsRecord.addMetadata("SamResourceId", "WebsiteDnsRecord");
+		CfnRecordSetGroup websiteDnsRecord = CfnRecordSetGroup.Builder.create(this, "WebsiteDnsRecord")
+				.hostedZoneId(dnsZoneId)
+				.recordSets(
+						List.of(
+								CfnRecordSetGroup.RecordSetProperty.builder()
+										.name(websiteDomain)
+										.type("A")
+										.aliasTarget(
+												CfnRecordSetGroup.AliasTargetProperty.builder()
+														.dnsName(websiteCloudFrontDistribution.getAttrDomainName())
+														.hostedZoneId("Z2FDTNDATAQYW2")
+														.evaluateTargetHealth(false)
+														.build()
+										)
+										.build()
+						)
+				)
+				.build();
 
-        this.websiteCloudFrontDistributionUrl = "https://" + websiteCloudFrontDistribution.getAttrDomainName() + "/";
-        CfnOutput.Builder.create(this, "CfnOutputWebsiteCloudFrontDistributionUrl")
-                .key("WebsiteCloudFrontDistributionUrl")
-                .value(this.websiteCloudFrontDistributionUrl.toString())
-                .description("Public URL of CloudFront Distribution.")
-                .build();
+		websiteDnsRecord.addMetadata("SamResourceId", "WebsiteDnsRecord");
 
-    }
+		this.websiteCloudFrontDistributionUrl = "https://" + websiteCloudFrontDistribution.getAttrDomainName() + "/";
+		CfnOutput.Builder.create(this, "CfnOutputWebsiteCloudFrontDistributionUrl")
+				.key("WebsiteCloudFrontDistributionUrl")
+				.value(this.websiteCloudFrontDistributionUrl.toString())
+				.description("Public URL of CloudFront Distribution.")
+				.build();
+
+	}
 }
